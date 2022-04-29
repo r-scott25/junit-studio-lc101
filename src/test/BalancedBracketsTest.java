6f70ade2ff745d2dd66cb5f4696245ef97270f37
r-scott25 <99931021+r-scott25@users.noreply.github.com>
2022-04-28 20:42:41 -0500

    balanced brackets

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import main.BalancedBrackets;
+
 public class BalancedBracketsTest {
 
     //TODO: add tests here
@@ -12,5 +14,47 @@ public class BalancedBracketsTest {
         assertEquals(true, true);
     }
 
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
 
+    @Test
+    public void stringInBracketsRetrunsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[LaunchCode]"));
+    }
+
+    @Test
+    public void splitStringBracektsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Launch[Code]"));
+    }
+    @Test
+    public void bracketAndStringReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]LaunchCode"));
+    }
+
+    @Test
+    public void balanceBracketWithNoBrackets() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
+
+    @Test hasTwoBalancedBracketsReturnsTrue() {
+    assertTrue(BalancedBrackets.hasBalancdedBrackets("[]LaunchCode[]"))
+    }
+}
+    @Test
+    public void oneOpeningBracketBeforeStringButNoClosingBracket() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[LaunchCode"));
+    }
+    @Test
+    public void stringClosingBracketStringOpeningBracket() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Launch]Code["));
+    }
+    @Test
+    public void justOneOpenBracket() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+    @Test
+    public void closeThenOpenBracket() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
 }
